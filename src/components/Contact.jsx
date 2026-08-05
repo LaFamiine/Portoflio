f9ca92782e5161b976a14620c0a89ae38d32a05a
Anastasia said <anastasiasaidfara@gmail.com>
2026-08-05 17:41:13 +0200

    template id modifier

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import { useRef, useState } from 'react'
 import emailjs from '@emailjs/browser'
 
 const SERVICE_ID = 'service_dg7t4xi'
-const TEMPLATE_ID = 'template_dg7t4xi'
+const TEMPLATE_ID = 'template_mavedzk'
 const PUBLIC_KEY = 'rQ5ic6tUtD9rsXOuS'
 
 function Contact() {
